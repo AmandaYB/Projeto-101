11df011c4b45d69d37de1ff7e529b3d49f16428e
AmandaYB <94137541+AmandaYB@users.noreply.github.com>
2023-05-24 11:25:59 -0300

    Add files via upload

---
+++ dado.py
@@ -0,0 +1,45 @@
+import random
+
+valor = random.randint(1, 6)
+
+if valor == 1:
+    print('[-----]')
+    print('[     ]')
+    print('[  0  ]')
+    print('[     ]')
+    print('[-----]')
+
+if valor == 2:
+    print('[-----]')
+    print('[ 0   ]')
+    print('[     ]')
+    print('[   0 ]')
+    print('[-----]')
+
+if valor == 3:
+    print('[-----]')
+    print('[ 0   ]')
+    print('[  0  ]')
+    print('[   0 ]')
+    print('[-----]')  
+
+if valor == 4:
+    print('[-----]')
+    print('[ 0 0 ]')
+    print('[     ]')
+    print('[ 0 0 ]')
+    print('[-----]')
+
+if valor == 5:
+    print('[-----]')
+    print('[ 0 0 ]')
+    print('[  0  ]')
+    print('[ 0 0 ]')
+    print('[-----]')
+
+if valor == 6:
+    print('[-----]')
+    print('[ 0 0 ]')
+    print('[ 0 0 ]')
+    print('[ 0 0 ]')
+    print('[-----]')
